3278d037e6aa238ad3686d8399b2e6ffcc475f0e
Ralph Recto <recto.ralph@gmail.com>
2016-04-28 17:54:50 -0400

    added more plugins

--- vim-config/init.sh
+++ vim-config/init.sh
@@ -11,7 +11,8 @@ git clone https://github.com/Lokaltog/vim-easymotion
 git clone https://github.com/kien/ctrlp.vim
 git clone https://github.com/tomasr/molokai
 git clone https://github.com/bling/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes
 git clone https://github.com/terryma/vim-multiple-cursors
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/scrooloose/nerdcommenter
-
+git clone https://github.com/ervandew/supertab
